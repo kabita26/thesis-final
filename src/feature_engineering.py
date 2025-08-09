b0fdc8a1ae4a47fa0f7c1c3689c24700d934448d
kabita26 <mahatok@gmail.com>
2025-08-09 18:22:09 +0545

    Update feature

--- src/feature_engineering.py
+++
@@ -1,7 +0,0 @@
-# src/feature_engineering.py
-
-def create_features(df):
-    # Example: create lag feature and interaction term
-    df['yield_per_temp'] = df['hg/ha_yield'] / df['avg_temp']
-    df['rainfall_x_pesticide'] = df['average_rain_fall_mm_per_year'] * df['pesticides_tonnes']
-    return df
